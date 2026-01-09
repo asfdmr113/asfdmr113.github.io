7ef916e06a6b89e843c1e74cc56643b8212e0f5d
asfdmr113 <damar.raditya113@gmail.com>
2026-01-09 10:01:02 +0000

    Deploying to gh-pages from @ asfdmr113/asfdmr113.github.io@fec0afc9c133d75d9ead91afa17344b81fd5a472 🚀

---
+++ assets/js/main.js
@@ -0,0 +1,30 @@
+// Place this at the end of your body or in a main.js file
+const themeToggleBtn = document.getElementById('theme-toggle');
+const darkIcon = document.getElementById('theme-toggle-dark-icon');
+const lightIcon = document.getElementById('theme-toggle-light-icon');
+
+// 1. Initial Theme Check
+if (localStorage.getItem('color-theme') === 'dark' ||
+    (!('color-theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
+    document.documentElement.classList.add('dark');
+    lightIcon.classList.remove('hidden');
+} else {
+    document.documentElement.classList.remove('dark');
+    darkIcon.classList.remove('hidden');
+}
+
+// 2. Toggle Click Handler
+themeToggleBtn.addEventListener('click', function() {
+    // Toggle icons
+    darkIcon.classList.toggle('hidden');
+    lightIcon.classList.toggle('hidden');
+
+    // If is currently dark mode
+    if (document.documentElement.classList.contains('dark')) {
+        document.documentElement.classList.remove('dark');
+        localStorage.setItem('color-theme', 'light');
+    } else {
+        document.documentElement.classList.add('dark');
+        localStorage.setItem('color-theme', 'dark');
+    }
+});
